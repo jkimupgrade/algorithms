7f06137be2259564a52d25e1b9a3ca8c6b019cc9
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-12-15 23:37:11 -0500

    completed findOdd function

--- codewars/findOdd.js
+++ codewars/findOdd.js
@@ -1,3 +1,24 @@
 // Given an array, find the integer that appears an odd number of times.
 // There will always be only one integer that appears an odd number of times.
+const findOdd = A => {
+  // store the counts of the numbers in an object
+  const obj = A.reduce((acc, cur) => {
+    if (acc[cur]) {
+      acc[cur]++
+    } else {
+      acc[cur] = 1;
+    }
+    return acc;
+  }, {});
+  // return the key that has an odd number for its value
+  for (key in obj) {
+    if (obj[key] % 2 !== 0) {
+      return Number(key);
+    }
+  }
+}
 
+
+// Test
+const test1 = findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5]);
+console.log('test1', test1); // -> 5
